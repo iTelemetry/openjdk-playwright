95cc5197581cf9478648bd41d79f24a6a8b7a06e
Harry Fox <1254033+hfoxy@users.noreply.github.com>
2022-03-29 00:41:43 +0100

    Add screenshot path

--- test-project/src/main/java/app/itelemetry/test/playwright/PlaywrightTestStart.java
+++ test-project/src/main/java/app/itelemetry/test/playwright/PlaywrightTestStart.java
@@ -6,13 +6,12 @@ import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 
 import java.io.File;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.nio.file.Path;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class PlaywrightTestStart {
 
-    public static void main(String[] args) throws URISyntaxException {
+    public static void main(String[] args) throws IOException {
         try (Playwright playwright = Playwright.create()) {
             System.out.println("Launched Playwright");
             try (Browser browser = playwright.chromium().launch()) {
@@ -26,11 +25,21 @@ public class PlaywrightTestStart {
                 page.navigate("https://google.com");
                 System.out.println("Navigated to google");
 
-                // File file = new File("image");
-                // Path path = file.toPath();
-
-                page.screenshot(new Page.ScreenshotOptions());
+                byte[] screenshot = page.screenshot(new Page.ScreenshotOptions());
                 System.out.println("Took screenshot");
+
+                File screenshotFolder = new File("screenshot");
+                if (!screenshotFolder.exists()) {
+                    if (!screenshotFolder.mkdir()) {
+                        throw new IOException("Unable to create directory");
+                    }
+                } else if (!screenshotFolder.isDirectory()) {
+                    throw new IOException("Path of screenshot folder already exists and is not a directory");
+                }
+
+                try (FileOutputStream fos = new FileOutputStream("screenshot/google.png")) {
+                    fos.write(screenshot);
+                }
 
                 page.close();
             }
